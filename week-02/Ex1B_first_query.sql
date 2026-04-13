b64d0acfdbba312b1f453dc58e61ba507f8ba1b2
sguerrero777 <sguero444@gmail.com>
2026-04-13 11:23:21 -0400

    added comment to first file in week-01

---
+++ week-02/Ex1B_first_query.sql
@@ -0,0 +1,3 @@
+/*
+Sakila is a databased used to collect information about a DVD rental business for use by management to track stores across inventory, loaned items, information about each DVD by category and staff behavior . 
+/*
