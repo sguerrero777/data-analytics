1d301dbc7262cd97ac9013a9cea65a945eb99921
sguerrero777 <sguero444@gmail.com>
2026-05-11 07:52:18 -0400

    Add 4C labs

---
+++ week-05/Ex4C_LoopScripts/loop_love.py
@@ -0,0 +1,27 @@
+# ==================================
+# Lab 1
+# Script 1: Luv Loop
+# ==================================
+
+# Create a list of words that mean great such as amazing, awesome, excellent, etc. 
+# Use a for loop to iterate through your list, and with each loop print out the phrase 
+# “Loops are ____!” filling in the blank with a word from the list. 
+# When your loop has finished, print the additional phrase “I <3 loops” 
+
+# ---------------------------------------------------------------
+
+# Final Script for Love Loops
+
+words = ['amazing', 'superb', 'excellent', 'awesome', 'fantastic']
+
+for i in words:
+    print(f'Loops are {i}!')
+print('I <3 loops')
+
+# Output:
+# Loops are amazing!
+# Loops are superb!
+# Loops are excellent!
+# Loops are awesome!
+# Loops are fantastic!
+# I <3 loops
